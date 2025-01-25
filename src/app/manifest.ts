652f0694cd19ce674b6eb0bddad0f680f0a7cc56
rinakin <sabrinachan.cs@gmail.com>
2025-01-24 18:15:27 -0800

    chore: update manifest and favicons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,8 +8,8 @@ export default function manifest(): MetadataRoute.Manifest {
       'Boost productivity with Pomoplant’s Pomodoro timer. Add and complete tasks in focused intervals, and watch your plant grow as you progress through your flow.',
     start_url: '/focus',
     display: 'standalone',
-    background_color: '#f2f5e0 ',
-    theme_color: '#c2c9b6',
+    theme_color: '#f2f5e0',
+    background_color: '#f2f5e0',
     icons: [
       {
         src: '/web-app-manifest-192x192.png',
@@ -20,6 +20,21 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/web-app-manifest-512x512.png',
         sizes: '512x512',
         type: 'image/png',
+      },
+    ],
+    screenshots: [
+      {
+        src: '/screenshot-app-705x925.png',
+        sizes: '705x945',
+        type: 'image/png',
+        label: 'Mobile App Overview',
+      },
+      {
+        src: '/screenshot-desktop-2000x1509.png',
+        sizes: '2000x1509',
+        type: 'image/png',
+        form_factor: 'wide',
+        label: 'Desktop App Overview',
       },
     ],
   };
